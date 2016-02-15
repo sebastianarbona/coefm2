5e0df2b945d5e70f59a4fbbbde599f8e66f5e16a
sebastianarbona <sebastian.arbona@gmail.com>
2016-02-14 23:33:06 -0300

    c2
    c3

--- www/js/index.js
+++ www/js/index.js
@@ -30,6 +30,12 @@ var app = {
         document.addEventListener('deviceready', function () {
             var Pushbots = PushbotsPlugin.initialize("56140447177959c4358b456a", {"android":{"sender_id":"185110731795"}});
 
+            setTimeout(function(){
+                console.log("Esperando...");
+            },20000);
+
+
+            Pushbots.debug(true);
 
             Pushbots.on("registered", function(token){
                 localStorage.setItem("token", token);
@@ -38,6 +44,8 @@ var app = {
             Pushbots.getRegistrationId(function(token){
                 localStorage.setItem("token", token);
             });
+
+
         }, false);
     },
     // deviceready Event Handler
